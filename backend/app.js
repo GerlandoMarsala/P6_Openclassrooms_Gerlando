7c97a9959ff0cc0595d35db0f6ae73b7dee1d284
GerlandoMarsala <gerlando.marsala@outlook.fr>
2022-03-11 13:00:00 +0100

    premier commit du projet 6

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+const app = express();
+
+const mongoose = require('mongoose');
+
+const userRoutes = require('./routes/user');
+
+mongoose.connect('mongodb+srv://gerlandomarsala:gerlando57@cluster0.dpvmn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Requête reçue !');
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({ message: 'Votre requête a bien été reçue !' });
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('Réponse envoyée avec succès !');
+});
+
+app.use('/api/auth', userRoutes);
+module.exports = app;
